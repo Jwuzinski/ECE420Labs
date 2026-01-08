66289386f9c19bc1065e15d390d146d02e870301
Julian Wuzinski <jwuzinsk@ualberta.ca>
2026-01-08 15:45:36 -0700

    updating template

--- Development-Kit-Lab1/matrix_multiplier.c
+++ Development-Kit-Lab1/matrix_multiplier.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <pthread.h>
+/* Usage: ./main <p> where p is the number of threads
 
-int main(){
-	
+*/
+int main(int argc, char *argv[]){
+	//Check for number of threads
+	//Error check if no threads are declared, maybe enforce a default?
+	if(argc != 2){
+		//Wrong number of args, decide what to do.
+		return -1;
+	}
+	//Create <p> threads
+
+	//Open matrix
+
+	//Assign threads to matrix
+
+	//Compute products
+
+	//Print results
 }
