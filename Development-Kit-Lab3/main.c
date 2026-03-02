25e7e432f9c8befc110e34305afa8c73c5dfb8e4
Mazen Khafagy <mazen.khafagi@gmail.com>
2026-03-02 11:16:48 -0700

    Added series testing

--- Development-Kit-Lab3/main.c
+++ Development-Kit-Lab3/main.c
@@ -5,6 +5,8 @@
 #include "Lab3IO.h"
 
 double **A;
+double *x;
+double **B; //TESTING MATRIX FOR SERIES
 int size;
 
 // Algorithm 1 of lab manual
@@ -54,9 +56,62 @@ void jordan_elimination() {
     }
 }
 
+void gaussian_elimination_series() {
+    for (int k = 0; k < size; k++) {
+        // PIVOTING: from row k to row n-1, find the row kp that has the maximum
+        // absolute value of the element in the kth column 
+        int big_row_index = k;
+        double big_value = 0;
+
+        for (int i = k; i < size; i++) {
+            if (fabs(B[i][k]) > fabs(big_value)) {
+                big_value = B[i][k];
+                big_row_index = i;
+            }
+        }
+
+        // After finding big_row swap current row with big_row
+        // used size+1 to change swap the b vector as well
+        double temp;
+        for (int col = 0; col < size+1; col++) {
+            // Swap elements at each column index
+            temp = B[k][col];
+            B[k][col] = B[big_row_index][col];
+            B[big_row_index][col] = temp;
+        }
+
+        // ELIMINATION
+        // TODO: DOUBLE CHECK
+        for (int row = k+1; row < size; row++) {
+            temp = B[row][k] / B[k][k];
+            for (int col = k; col < size+1; col++) {
+                B[row][col] -= (temp*B[k][col]);
+            }
+        }
+    }
+}
+
+void jordan_elimination_series() {
+    for (int k = size-1; k > 0; k--) {
+        for (int row = 0; row < k; row++) {
+            B[row][size] -= (B[row][k] / B[k][k]) * B[k][size];
+            B[row][k] = 0;
+        }
+    }
+}
+
 int main() {
     // Load the input data
     if (Lab3LoadInput(&A, &size) != 0) {
+        printf("ERROR: Couldn't load matrix");
+        return 1;
+    }
+
+    // Create output vector (x)
+    x = CreateVec(size);
+
+    // TODO: TESTING REMOVE LATER
+    if (Lab3LoadInput(&B, &size) != 0) {
         printf("ERROR: Couldn't load matrix");
         return 1;
     }
@@ -65,6 +120,28 @@ int main() {
     gaussian_elimination();
     jordan_elimination();
 
-    // print result
+    // TODO: TESTING REMOVE LATER
+    gaussian_elimination_series();
+    jordan_elimination_series();
+
+    // print result FOR TESTING
     PrintMat(A, size, size+1);
+
+    // TESTING MATRICES A(PARALLEL) AND B(SERIES) OUTPUTS
+    int mismatch_counter = 0;
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (A[row][col] != B[row][col]) {
+                mismatch_counter++;
+            }
+        }
+    }
+    printf("NUMBER OF MISMATCHS: %d\n", mismatch_counter);
+
+    // TODO: IMPLEMENT TIMER
+    double time = 0.0;
+
+    // TODO: MAKE x BY DIVIDING b BY A
+    // b IS STORED IN A[][size] 
+    Lab3SaveOutput(x, size, time);
 }
