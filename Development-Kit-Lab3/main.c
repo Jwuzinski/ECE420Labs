2ddc5849944cf99b838031ae4f2a35e95dc130e9
Ghassan <ebrabish@ualberta.ca>
2026-03-02 13:09:58 -0700

    added the openmp repo

--- Development-Kit-Lab3/main.c
+++ Development-Kit-Lab3/main.c
@@ -12,55 +12,68 @@ int size;
 
 // Algorithm 1 of lab manual
 void gaussian_elimination() {
-    // # pragma omp parallel num_threads(thread_count)
-    {
-        for (int k = 0; k < size; k++) {
-            // PIVOTING: from row k to row n-1, find the row kp that has the maximum
-            // absolute value of the element in the kth column 
-            int big_row_index = k;
-            double big_value = 0;
+    // double START, EDD, ELAPSED;
+    // START = omp_get_wtime();
+    // PIVOTING + ELIMINATION
+    for (int k = 0; k < size; k++) {
+        // PIVOTING: from row k to row n-1, find the row kp that has the maximum
+        // absolute value of the element in the kth column 
+        int big_row_index = k;
+        double big_value = 0;
 
-            for (int i = k; i < size; i++) {
-                if (fabs(A[i][k]) > fabs(big_value)) {
-                    big_value = A[i][k];
-                    big_row_index = i;
-                }
+        for (int i = k; i < size; i++) {
+            if (fabs(A[i][k]) > fabs(big_value)) {
+                big_value = A[i][k];
+                big_row_index = i;
             }
+        }
 
-            // After finding big_row swap current row with big_row
-            // used size+1 to change swap the b vector as well
-            double temp;
-            for (int col = 0; col < size+1; col++) {
-                // Swap elements at each column index
-                temp = A[k][col];
-                A[k][col] = A[big_row_index][col];
-                A[big_row_index][col] = temp;
-            }
+        // After finding big_row swap current row with big_row
+        // used size+1 to change swap the b vector as well
+        double temp;
+        for (int col = 0; col < size+1; col++) {
+            // Swap elements at each column index
+            temp = A[k][col];
+            A[k][col] = A[big_row_index][col];
+            A[big_row_index][col] = temp;
+        }
 
-            // ELIMINATION
-            // TODO: DOUBLE CHECK
-            for (int row = k+1; row < size; row++) {
-                temp = A[row][k] / A[k][k];
-                for (int col = k; col < size+1; col++) {
-                    A[row][col] -= (temp*A[k][col]);
-                }
+        // ELIMINATION
+        #pragma omp parallel for num_threads(thread_count) private(temp)
+        for (int row = k+1; row < size; row++) {
+            temp = A[row][k] / A[k][k];
+            for (int col = k; col < size+1; col++) {
+                A[row][col] -= (temp*A[k][col]);
             }
         }
     }
+    
+    // EDD = omp_get_wtime();
+    // ELAPSED = EDD - START;
+    // printf("TIME ELAPSED: %f seconds\n", ELAPSED);
 }
 
 // Algorithm 2 of lab manual
 // TODO: DOUBLE CHECK
 void jordan_elimination() {
+    // double START, EDD, ELAPSED;
+    // START = omp_get_wtime();
     for (int k = size-1; k > 0; k--) {
+        #pragma omp parallel for num_threads(thread_count)
         for (int row = 0; row < k; row++) {
             A[row][size] -= (A[row][k] / A[k][k]) * A[k][size];
             A[row][k] = 0;
         }
     }
+    // EDD = omp_get_wtime();
+    // ELAPSED = EDD - START;
+    // printf("TIME ELAPSED: Jordan Elimination %f seconds\n", ELAPSED);
 }
 
 void gaussian_elimination_series() {
+    // double START, EDD, ELAPSED;
+    // START = omp_get_wtime();    
+
     for (int k = 0; k < size; k++) {
         // PIVOTING: from row k to row n-1, find the row kp that has the maximum
         // absolute value of the element in the kth column 
@@ -93,6 +106,9 @@ void gaussian_elimination_series() {
             }
         }
     }
+    // EDD = omp_get_wtime();
+    // ELAPSED = EDD - START;
+    // printf("SERIES TIME ELAPSED: GAussian %f seconds\n", ELAPSED);
 }
 
 void jordan_elimination_series() {
@@ -127,15 +143,27 @@ int main(int argc, char* argv[]) {
     }
 
     // DO STUFF
+    double START, EDD, ELAPSED;
+    START = omp_get_wtime();
+    // Parallel functions    
     gaussian_elimination();
     jordan_elimination();
+    EDD = omp_get_wtime();
+    ELAPSED = EDD - START;
+    printf("TOTAL PARALLEL TIME ELAPSED: %f seconds\n", ELAPSED);
 
     // TODO: TESTING REMOVE LATER
+    double START_SERIES, EDD_SERIES, ELAPSED_SERIES;
+    START_SERIES = omp_get_wtime();
+    // Series functions
     gaussian_elimination_series();
     jordan_elimination_series();
+    EDD_SERIES = omp_get_wtime();
+    ELAPSED_SERIES = EDD_SERIES - START_SERIES;
+    printf("TOTAL SERIES TIME ELAPSED: %f seconds\n", ELAPSED_SERIES);
 
     // print result FOR TESTING
-    PrintMat(A, size, size+1);
+    // PrintMat(A, size, size+1);
 
     // TESTING MATRICES A(PARALLEL) AND B(SERIES) OUTPUTS
     int mismatch_counter = 0;
