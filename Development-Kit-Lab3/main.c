ccc0c49a9f3e2c5b450f30eedbefbd12fa07dec8
Julian Wuzinski <jwuzinsk@ualberta.ca>
2026-03-04 15:59:33 -0700

    Performance enhancements and timer fix

--- Development-Kit-Lab3/main.c
+++ Development-Kit-Lab3/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include "timer.h"
 #include "Lab3IO.h"
 
 double **A;
@@ -15,39 +16,45 @@ void gaussian_elimination() {
     // double START, EDD, ELAPSED;
     // START = omp_get_wtime();
     // PIVOTING + ELIMINATION
+    #pragma omp parallel num_threads(thread_count) default(none) shared(A, size)
+    {
     for (int k = 0; k < size; k++) {
         // PIVOTING: from row k to row n-1, find the row kp that has the maximum
         // absolute value of the element in the kth column 
-        int big_row_index = k;
-        double big_value = 0;
+        #pragma omp single
+        {
+        	int big_row_index = k;
+        	double big_value = 0;
 
-        for (int i = k; i < size; i++) {
-            if (fabs(A[i][k]) > fabs(big_value)) {
-                big_value = A[i][k];
-                big_row_index = i;
-            }
-        }
+        	for (int i = k; i < size; i++) {
+            		if (fabs(A[i][k]) > fabs(big_value)) {
+                		big_value = A[i][k];
+                		big_row_index = i;
+            		}
+        	}
 
         // After finding big_row swap current row with big_row
         // used size+1 to change swap the b vector as well
-        double temp;
-        for (int col = 0; col < size+1; col++) {
-            // Swap elements at each column index
-            temp = A[k][col];
-            A[k][col] = A[big_row_index][col];
-            A[big_row_index][col] = temp;
+        	double temp;
+        	for (int col = 0; col < size+1; col++) {
+            	// Swap elements at each column index
+            		temp = A[k][col];
+            		A[k][col] = A[big_row_index][col];
+            		A[big_row_index][col] = temp;
+        	}
         }
-
+	#pragma omp barrier
         // ELIMINATION
-        #pragma omp parallel for num_threads(thread_count) private(temp)
+        #pragma omp for
         for (int row = k+1; row < size; row++) {
-            temp = A[row][k] / A[k][k];
+            double temp = A[row][k] / A[k][k];
             for (int col = k; col < size+1; col++) {
                 A[row][col] -= (temp*A[k][col]);
             }
         }
+	#pragma omp barrier
     }
-    
+  }
     // EDD = omp_get_wtime();
     // ELAPSED = EDD - START;
     // printf("TIME ELAPSED: %f seconds\n", ELAPSED);
@@ -58,13 +65,17 @@ void gaussian_elimination() {
 void jordan_elimination() {
     // double START, EDD, ELAPSED;
     // START = omp_get_wtime();
+   #pragma omp parallel num_threads(thread_count) default(none) shared(A, size)
+   {
     for (int k = size-1; k > 0; k--) {
-        #pragma omp parallel for num_threads(thread_count)
+	#pragma omp for
         for (int row = 0; row < k; row++) {
             A[row][size] -= (A[row][k] / A[k][k]) * A[k][size];
             A[row][k] = 0;
         }
+	#pragma omp barrier
     }
+   }
     // EDD = omp_get_wtime();
     // ELAPSED = EDD - START;
     // printf("TIME ELAPSED: Jordan Elimination %f seconds\n", ELAPSED);
@@ -144,21 +155,21 @@ int main(int argc, char* argv[]) {
 
     // DO STUFF
     double START, EDD, ELAPSED;
-    START = omp_get_wtime();
+    GET_TIME(START);
     // Parallel functions    
     gaussian_elimination();
     jordan_elimination();
-    EDD = omp_get_wtime();
+    GET_TIME(EDD);
     ELAPSED = EDD - START;
     printf("TOTAL PARALLEL TIME ELAPSED: %f seconds\n", ELAPSED);
 
     // TODO: TESTING REMOVE LATER
     double START_SERIES, EDD_SERIES, ELAPSED_SERIES;
-    START_SERIES = omp_get_wtime();
+    GET_TIME(START_SERIES);
     // Series functions
     gaussian_elimination_series();
     jordan_elimination_series();
-    EDD_SERIES = omp_get_wtime();
+    GET_TIME(EDD_SERIES);
     ELAPSED_SERIES = EDD_SERIES - START_SERIES;
     printf("TOTAL SERIES TIME ELAPSED: %f seconds\n", ELAPSED_SERIES);
 
@@ -178,7 +189,10 @@ int main(int argc, char* argv[]) {
 
     // TODO: IMPLEMENT TIMER
     double time = 0.0;
-
+    time = ELAPSED;
+    for(int i = 0; i < size; i++){
+	x[i] = A[i][size] / A[i][i];
+    }
     // TODO: MAKE x BY DIVIDING b BY A
     // b IS STORED IN A[][size]
     // MAYBE DO IT IN THE JORDAN ELIMINATION STAGE???
